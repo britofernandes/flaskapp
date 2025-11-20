78a360663fe4dcb669bd9d6c9d98652bf0b81ed8
britofernandes <brito.fernandes@aluno.ifsp.edu.br>
2025-11-20 19:53:11 -0300

    main estava errado

--- app/main/views.py
+++ app/main/views.py
@@ -2,6 +2,10 @@ from flask import render_template
 from . import main
 from ..models import User
 
+@main.route('/')
+def index():
+    return render_template('index.html')
+
 @main.route('/usuarios')
 def usuarios():
     users = User.query.all()
