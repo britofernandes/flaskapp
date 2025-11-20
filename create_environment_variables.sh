cbf332cd0ed04c0071fd95578ff60a6e0954d88b
britofernandes <brito.fernandes@aluno.ifsp.edu.br>
2025-11-20 17:47:04 -0300

    save

--- create_environment_variables.sh
+++ create_environment_variables.sh
@@ -2,5 +2,5 @@ export FLASK_APP=flasky.py
 export FLASK_DEBUG=1
 export MAIL_USERNAME=flaskaulas@zohomail.com
 export MAIL_PASSWORD=$1
-export FLASKY_ADMIN=fabio.teixeira@ifsp.edu.br
+export FLASKY_ADMIN=brito.fernandes@ifsp.edu.br
 export FLASKY_CONFIG=DevelopmentConfig
