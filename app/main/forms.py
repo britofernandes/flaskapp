8ad1eba2e32910a3151bc8750a111bccb619f752
britofernandes <brito.fernandes@aluno.ifsp.edu.br>
2025-11-25 17:20:11 -0300

    com formulario

--- app/main/forms.py
+++ app/main/forms.py
@@ -6,3 +6,23 @@ from wtforms.validators import DataRequired
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+class CadastroForm(FlaskForm):
+    nome = StringField('Nome', validators=[DataRequired()])
+    sobrenome = StringField('Sobrenome', validators=[DataRequired()])
+    instituicao = StringField('Instituição', validators=[DataRequired()])
+
+    disciplinas = SelectMultipleField(
+        'Disciplinas',
+        choices=[
+            ('matematica', 'Matemática'),
+            ('fisica', 'Física'),
+            ('quimica', 'Química'),
+            ('biologia', 'Biologia'),
+            ('portugues', 'Português'),
+            ('historia', 'História'),
+        ],
+        validators=[DataRequired()]
+    )
+
+    submit = SubmitField('Enviar')
--- app/main/views.py
+++ app/main/views.py
@@ -6,6 +6,23 @@ from ..models import User
 def index():
     return render_template('index.html')
 
+
+@main.route('/formulario', methods=['GET', 'POST'])
+def formulario():
+    form = CadastroForm()
+    resultados = None
+
+    if form.validate_on_submit():
+        resultados = {
+            'nome': form.nome.data,
+            'sobrenome': form.sobrenome.data,
+            'instituicao': form.instituicao.data,
+            'disciplinas': form.disciplinas.data,
+        }
+
+    return render_template('formulario.html', form=form, resultados=resultados)
+
+
 @main.route('/usuarios')
 def usuarios():
     users = User.query.all()
